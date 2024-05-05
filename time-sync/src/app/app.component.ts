98e0425ecd645b922c2a69f7ae8e31b777619360
doahusre <dhowa1@gmail.com>
2024-05-05 01:33:00 -0700

    hookup

--- time-sync/src/app/app.component.ts
+++ time-sync/src/app/app.component.ts
@@ -5,16 +5,18 @@ import { DataService } from './data.service';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HttpClientModule, DataService],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'time-sync';
+  totViews = 10;
   constructor(private dataService: DataService) {} // Inject DataService instead of HttpClient
   fetchData() {
     this.dataService.getValues().subscribe(data => { // Use the injected service
       console.log(data);
+      this.totViews = data;
     });
   }
 }
